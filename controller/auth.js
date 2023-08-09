81c50f1fe584ad9bc47a0463b75ccd7a4845c820
Vita Nychvoloda <vitanychvoloda@yahoo.com>
2023-08-09 23:58:33 +0300

    Added avatar and unit test for login

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,15 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { SECRET_KEY } = process.env;
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,7 +20,12 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
     password: newUser.password,
     email: newUser.email,
@@ -62,10 +73,37 @@ const updateSubscriptionUser = async (req, res) => {
   res.json({ _id, subscription });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  if (!_id) {
+    throw HttpError(401);
+  }
+  const { path: tempDir, originalname } = req.file;
+
+  Jimp.read(tempDir)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write(resultUpload);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarDir, filename);
+  await fs.rename(tempDir, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  console.log(resultUpload);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscriptionUser: ctrlWrapper(updateSubscriptionUser),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
